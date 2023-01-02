38c7ab8409368d26885a101bb0aabcc1bf2246b5
Kaustubh Aphale <kaustubh.aphale@ucdconnect.ie>
2023-01-02 21:00:56 +0000

    UI changes and client java changes

--- DS/client/src/main/java/service/Client.java
+++ DS/client/src/main/java/service/Client.java
@@ -4,21 +4,26 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 
 @RestController
 public class Client {
     public static int ID = 0;
 
-    @RequestMapping(value = "/request", method = RequestMethod.GET)
-    public ResponseEntity getQuotations() {
-        System.out.println("here");
+    @RequestMapping(value = "/request", method = RequestMethod.POST)
+    public ResponseEntity getQuotations(@RequestBody MultipartFile file) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<MultipartFile> request = new HttpEntity<>(file);
         HttpHeaders headers = new HttpHeaders();
+        restTemplate.postForObject("http://localhost:8081/broker", request, MultipartFile.class);
         return new ResponseEntity<>("test", headers, HttpStatus.CREATED);
     }
 
-    //	To manage a runtime exception i.e. if an unknown URI path is passed
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public class NoSuchQuotationException extends RuntimeException {
-        static final long serialVersionUID = -6516152229878843037L;
+    public class NoSuchMachine extends RuntimeException {
+        static final int error_id = -1;
     }
 }
